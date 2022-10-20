5f4dab2baad4ed0ff36d5b9e584b6920063344ae
Andrey-Legion <andrey.legion@gmail.com>
2022-10-20 19:36:50 +0300

    ДЗ Строки Часть1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Task1");
+        String firstName = "Ivan", middleName = "Ivanovich", lastName = "Ivanov";
+        String fullName=lastName+" "+firstName+" "+middleName;
+        System.out.println("ФИО сотрудника- "+fullName);
+
+        System.out.println("Task2");
+        String fullNameUpper=fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчёта- "+fullNameUpper);
+
+        System.out.println("Task3");
+        fullName = "Иванов Семён Семёнович";
+        String fullNameCorrect=fullName.replace('ё','е');
+        System.out.println("Данные ФИО сотрудника- "+fullNameCorrect);
+
+        System.out.println("Task3-1");
+        char[] changeOfSymbol=fullName.toCharArray();
+        for (int i = 0; i < changeOfSymbol.length-1; i++) {
+            if(changeOfSymbol[i]=='ё') {
+                changeOfSymbol[i] = 'е';
+            }
+        }
+        //String fullNameNew=new String(changeOfSymbol);
+        System.out.println("Данные ФИО сотрудника- "+ new String(changeOfSymbol));
+
 
 
     }
